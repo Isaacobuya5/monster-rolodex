9604ec3ab7ad88c00919a4149b56a07aa7eb3544
Isaac Obuya <Szack4477@gmail.com>
2019-10-31 23:03:53 +0300

    Updates

---
+++ precache-manifest.9403cbeed34fc132baf737b9d5715cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449cc0b5c40a346db7ba801330d8f2a4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5eb1514f2ddebe0041ca",
+    "url": "/monster-rolodex/static/css/main.e0801681.chunk.css"
+  },
+  {
+    "revision": "4a56a8b28e8e160be497",
+    "url": "/monster-rolodex/static/js/2.2f3b1152.chunk.js"
+  },
+  {
+    "revision": "5eb1514f2ddebe0041ca",
+    "url": "/monster-rolodex/static/js/main.85060e4f.chunk.js"
+  },
+  {
+    "revision": "b34a37fda6586bb865e1",
+    "url": "/monster-rolodex/static/js/runtime-main.cf62df5d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monster-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
